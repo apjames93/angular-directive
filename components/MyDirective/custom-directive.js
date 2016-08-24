1e1d0d5e7aed327c98f4e8ce46ec87ae2d9a943f
Alexander James <AlexJames@Alexanders-MacBook-Air.local>
2016-08-23 18:23:13 -0600

    added notes

--- components/MyDirective/custom-directive.js
+++ components/MyDirective/custom-directive.js
@@ -1,16 +1,25 @@
+//make iif
 (function(){
+  //set up module for angular
   angular
     .module('custom.MyDirective.custom-directive', [])
+    //make the directive name
     .directive('customDirective', customDirective);
-
+    //make fuction to return the directive
     function customDirective(){
       var directive = {
+        //restrict to a dom element
         restrict: 'E',
+        //html that the directive will use
         templateUrl: '/templates/custom-directive.html',
-        scope: { },
+        //makeing isolate scope for the directive
+        scope: {},
+        //adds a controller for the directive to use to pass scope around
         controller: directiveController,
+        //pass the controller as for when the file is minify
         controllerAs: 'directiveController'
       };
+      //return the directive that we made 
       return directive;
 }
 directiveController.$inject = [];
